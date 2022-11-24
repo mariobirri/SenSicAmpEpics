fdec4a476c04d1f2c0c5fe64b4df4959e4a2e2da
mariobirri <mario.birri@psi.ch>
2022-11-24 10:14:24 +0100

    moved programm into diferent files

--- SenSicAmp_Socket.py
+++ SenSicAmp_Socket.py
@@ -1,2 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 24 09:56:41 2022
 
+@author: birri_m
+"""
+
+#!/usr/bin/env python
+import socket # for sockets
+import SenSicAmp_data as data
+import sys
+import time
+
+remote_ip = "129.129.130.210" # should match the instrument IP address
+port = 3000 # the port number of the instrument service
+
+# Socket connection
+#-------------------
+def SocketConnect():
+    try:
+        #create an AF_INET, STREAM socket (TCP)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print('Socket connected')
+    except socket.error:
+        print('Failed to create socket.')
+        sys.exit()
+    try:
+        #Connect to remote server
+        s.connect((remote_ip, port))
+    except socket.error:
+        print('failed to connect to ip ' + remote_ip)
+    return s
+
+# Socket send command
+#--------------------
+def SocketQuery(Sock, cmd):
+    try:
+        # Send cmd string
+        Sock.sendall(cmd)
+        # and wait
+        time.sleep(0.1)
+    except socket.error:
+        #Send failed
+        print('Send failed')
+        sys.exit()
+    reply = Sock.recv(4096)
+    #print(reply)
+    return reply
+
+# Socket close
+#--------------
+def SocketClose(Sock):
+    #close the socket
+    Sock.close()
+    time.sleep(1)
+#-------------------------------------------------------------------------
+
+s = SocketConnect()
+val = SocketQuery(s, data.startAmp)
+print(val) 
+time.sleep(1)
+val = SocketQuery(s, data.setGain)
+print(val)
+SocketClose(s)
 
--- SenSicAmp_data.py
+++ SenSicAmp_data.py
@@ -1,2 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 24 09:56:41 2022
+
+@author: birri_m
+"""
+
+# global strings
+startAmp = b'setdac:50'
+setGain = b'setgainmode:8;4;2;2'
 
 
